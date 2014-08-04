9d3266c5b722302ca4626ec005b839f5a71553d7
sakashushu <sakashushu@gmail.com>
2014-08-04 15:55:39 +0900

    DB接続先をH2のPostgreSQLモードにした。　等

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -5,11 +5,6 @@ create table HaUser (
   name character varying(255) NOT NULL,
   password character varying(255) NOT NULL,
   CONSTRAINT hauser_pkey PRIMARY KEY (hauser_id)
-)
-WITH (
-  OIDS=FALSE
 );
-ALTER TABLE HaUser
-  OWNER TO postgres;
 # --- !Downs
 drop table if exists HaUser;
